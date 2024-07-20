4cc1e23bf334fbc9e61fd72e8a12041a854d8e7b
Morys5 <murilo.mv4321@usp.br>
2024-07-20 16:25:28 -0300

    Primeiro commit

---
+++ lisa_ws/build/estrutura/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lisa/lisa_ws/src/estrutura
+# Build directory: /home/lisa/lisa_ws/build/estrutura
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
